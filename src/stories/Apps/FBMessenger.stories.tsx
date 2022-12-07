460791e17e4343709402dd8a2a86552b772c52b7
Gustavo <valbuena52@gmail.com>
2022-12-07 12:39:23 -0300

    add some storybook params for FBMessenger

--- src/stories/Apps/FBMessenger.stories.tsx
+++ src/stories/Apps/FBMessenger.stories.tsx
@@ -24,9 +24,9 @@ const Template: ComponentStory<typeof FBMessenger> = (args) => <FBMessenger {...
 
 export const FBMessengerDefault = Template.bind({})
 FBMessengerDefault.args = {
-  phone: '+5491131312222',
+  fullname: 'Lionel Messi',
   content: new DOMParser().parseFromString(
-    '<div id="dynamic-content"><div data-position="0" id="component-text-1"><h2>Text</h2><p>Body</p></div><div data-position="0" id="component-text-2"><h2>Text1231</h2><p>Body</p></div></div>',
+    '<div id="dynamic-content"><div data-position="0" id="component-text-1"><h2>Text</h2><p>Body</p></div><div data-position="0" id="component-text-2"><h2>Text1231</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam arcu ex, venenatis sed ultrices vitae, sollicitudin sit amet sapien. Phasellus molestie turpis nec urna porttitor bibendum. Aenean orci nisl, congue et diam eu, vestibulum mattis ipsum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam in lorem sed lacus mollis bibendum. Nulla iaculis commodo tristique. Pellentesque at sapien fermentum, sollicitudin tortor a, pulvinar nisi. Maecenas quis gravida enim. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque massa odio, dapibus eu tincidunt scelerisque, aliquet non sem. Nam a ex odio. Etiam lacus odio, tristique eu ornare quis, iaculis quis lacus. Suspendisse vel molestie purus. Phasellus dignissim consectetur metus at facilisis.</p></div></div>',
     'text/html'
   )
 }
