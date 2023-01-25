0f7037ab8e4658d05924844ae056c320defa44a8
juandans01 <juandans01@gmail.com>
2023-01-25 12:14:41 -0300

    add icons to new fields of work

---
+++ src/components/UI/WorkFieldItem/assets/Service.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="40"
+      height="40"
+      fill="none"
+      viewBox="0 0 40 40"
+    >
+      <path
+        stroke="#fff"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M30 13.333h1.667a6.667 6.667 0 010 13.333H30"
+      ></path>
+      <path
+        stroke="#111"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M3.333 13.333H30v15A6.667 6.667 0 0123.333 35H10a6.667 6.667 0 01-6.667-6.667v-15zM10 1.667v5M16.667 1.667v5M23.333 1.667v5"
+      ></path>
+    </svg>
+  );
+}
+
+export default Icon;
